2faccc5bc00043ccb31c290c17f58d8cc4202846
gopicci <56777940+gopicci@users.noreply.github.com>
2020-07-27 09:58:24 -0700

    Raising input root box

--- web/src/components/Create.js
+++ web/src/components/Create.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
-import {Box} from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import localforage from "localforage";
 import CaptureImage from "./create/CaptureImage";
 import InputRouter from "./create/InputRouter";
