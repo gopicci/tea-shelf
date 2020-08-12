146de851e4d563ccfdb66dff4c3b2369b0053c0d
gopicci <56777940+gopicci@users.noreply.github.com>
2020-08-12 14:36:01 -0700

    Adding origin map

--- web/src/components/details/mobile/DetailsCardOrigin.js
+++ web/src/components/details/mobile/DetailsCardOrigin.js
@@ -3,15 +3,30 @@ import { Box, Card, Typography } from "@material-ui/core";
 import ReactCountryFlag from "react-country-flag";
 import { makeStyles } from "@material-ui/core/styles";
 import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+} from "react-simple-maps";
+import {
   getCountryCode,
   getOriginName,
 } from "../../../services/ParsingService";
 import { detailsMobileStyles } from "../../../style/DetailsMobileStyles";
+import { geography } from "../../../services/Geography";
 
 const useStyles = makeStyles((theme) => ({
+  name: {
+    margin: theme.spacing(1),
+  },
   countryFlag: {
     paddingLeft: theme.spacing(0.5),
     margin: "auto",
+  },
+  mapBox: {
+    flexGrow: 1,
+    margin: theme.spacing(2),
+    marginTop: 0,
   },
 }));
 
@@ -24,13 +39,19 @@ export default function DetailsCardOrigin({ teaData }) {
   const classes = useStyles();
   const detailsClasses = detailsMobileStyles();
 
+  let coordinates;
+  if (teaData.origin.longitude && teaData.origin.latitude)
+    coordinates = [teaData.origin.longitude, teaData.origin.latitude];
+
   return (
     <Card className={detailsClasses.card}>
       <Box className={detailsClasses.genericBox}>
         <Typography variant="caption" display="block">
           Origin:
         </Typography>
-        <Typography variant="body2">{getOriginName(teaData.origin)}</Typography>
+        <Typography variant="body2" className={classes.name}>
+          {getOriginName(teaData.origin)}
+        </Typography>
         <ReactCountryFlag
           svg
           style={{
@@ -43,6 +64,37 @@ export default function DetailsCardOrigin({ teaData }) {
           aria-label={teaData.origin.country}
         />
       </Box>
+      {coordinates && (
+        <Box className={detailsClasses.mapBox}>
+          <ComposableMap
+            projection="geoMercator"
+            projectionConfig={{
+              scale: 400,
+              center: coordinates,
+            }}
+          >
+            <Geographies geography={geography}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={
+                      geo.properties.NAME === teaData.origin.country
+                        ? "#ccc"
+                        : "#EAEAEC"
+                    }
+                    stroke="#D6D6DA"
+                  />
+                ))
+              }
+            </Geographies>
+            <Marker key="marker" coordinates={coordinates}>
+              <circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
+            </Marker>
+          </ComposableMap>
+        </Box>
+      )}
     </Card>
   );
 }
