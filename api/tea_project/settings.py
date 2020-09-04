0cb96fa99f22909a0a1d057d87bb51ccf3b13668
gopicci <56777940+gopicci@users.noreply.github.com>
2020-09-04 09:28:35 -0700

    DRF admin auth fix

--- api/tea_project/settings.py
+++ api/tea_project/settings.py
@@ -156,8 +156,8 @@ if PROJECT_ID:
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
