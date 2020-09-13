cc229d157969d2ca7e8700e8185bcf3532735db4
gopicci <56777940+gopicci@users.noreply.github.com>
2020-09-13 10:23:59 -0700

    Defining password reset

--- api/catalog/views.py
+++ api/catalog/views.py
@@ -89,11 +89,10 @@ def password_reset_token_created(sender, reset_password_token, *args, **kwargs):
     :param kwargs:
     :return:
     """
-    reset_url = reverse("password_reset:reset-password-request")
 
     context = {
         "email": reset_password_token.user.email,
-        "reset_password_url": f"{settings.BASE_URL}{reset_url}?reset_token={reset_password_token.key}",
+        "reset_password_url": f"{settings.BASE_URL}/?reset_token={reset_password_token.key}",
     }
 
     email_html_message = render_to_string("user_reset_password.txt", context)
